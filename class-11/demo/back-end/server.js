461fe29046986e7fe9030aca34a154e6e936e37e
JB Tellez <jb.tellez@gmail.com>
2023-12-11 14:15:10 -0800

    adds db url notes and sample

--- class-11/demo/back-end/server.js
+++ class-11/demo/back-end/server.js
@@ -1,14 +1,17 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
+const mongoose = require('mongoose');
+// TODO: get from environment
+mongoose.connect('mongodb://127.0.0.1:27017/cats');
+
+const PORT = process.env.PORT || 3005;
 const Cat = require('./models/cat');
 
-const PORT = process.env.PORT;
 const app = express();
 
+// middleware
 app.use(cors());
-mongoose.connect(process.env.DATABASE_URL);
 
 app.get('/cats', async (request, response) => {
 
@@ -20,7 +23,7 @@ app.get('/cats', async (request, response) => {
 
   const cats = await Cat.find(filterQuery);
 
-  response.send(cats);
+  response.json(cats);
 });
 
-app.listen(PORT, () => console.log('Listening on PORT', PORT));
+app.listen(PORT, () => console.log('Listening on PORT:', PORT));
