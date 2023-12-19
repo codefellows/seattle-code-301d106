6ca12f9b460378eb710225f43d1db2bf514f06d5
JB Tellez <jb.tellez@gmail.com>
2023-12-19 13:57:21 -0800

    adds user extraction

--- class-15/demo/auth-back/server.js
+++ class-15/demo/auth-back/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const { auth } = require('express-oauth2-jwt-bearer');
+const axios = require('axios');
 
 const app = express();
 app.use(cors());
@@ -13,14 +14,21 @@ const jwtCheck = auth({
   tokenSigningAlg: process.env.AUTH_TOKEN_SIGNING_ALG,
 });
 
+const jwtUser = async (req, res, next) => {
+  const response = await axios.get(req.auth.payload.aud[1], { headers: { 'Authorization': req.headers.authorization } });
+  req.user = response.data;
+  next();
+};
+
 // enforce on all endpoints
 app.use(jwtCheck);
+app.use(jwtUser);
 
 
 const PORT = process.env.PORT || 3000;
 
 app.get('/ping', (req, res) => {
-  res.send('pong');
+  res.send('pong - ' + req.user.email);
 });
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
